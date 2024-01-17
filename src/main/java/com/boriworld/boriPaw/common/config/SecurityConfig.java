5aded3406a845ce6949afaf77c2fcf9c673ac3a3
cszzang89@gamail.com <cszzang89@gmail.com>
2024-01-17 23:44:19 +0900

    update CORS Config

--- src/main/java/com/boriworld/boriPaw/common/config/SecurityConfig.java
+++ src/main/java/com/boriworld/boriPaw/common/config/SecurityConfig.java
@@ -74,7 +74,6 @@ public class SecurityConfig {
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
-//        configuration.setMaxAge(3600L); //1시간
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
